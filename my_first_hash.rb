5171baa7020a52517a955ecb8c2eb0abf2b725c6
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-19 02:31:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+  my_hash = [name: "Caleb"]
   
 end
 
@@ -11,6 +11,10 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
+  the_manifest = [
+      "whale bone corset: "5", 
+      "porcelain vases:"
+    ]
 
 end
 
