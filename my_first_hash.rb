d4710aae95b5c818157d60dc806fe220abe58b1d
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-19 02:38:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  my_hash = ["name" => "Caleb"]
+  my_hash = {"name" => "Caleb", "age" => 25}
   
 end
 
@@ -11,11 +11,11 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
-  the_manifest = [
+  the_manifest = {
       "whale bone corset" => 5, 
       "porcelain vases" => 2,
       "oil paintings" => 3
-    ]
+    }
 
 end
 
@@ -27,7 +27,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest
 end
 
 def adding
